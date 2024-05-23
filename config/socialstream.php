d78afb834e1263118a05797a7ac291c0784fddcd
Nathalie Castells-Brooke <nathalie.castells@rothamsted.ac.uk>
2024-05-23 13:11:36 +0100

    Base: social stream and livewire - working

---
+++ config/socialstream.php
@@ -0,0 +1,13 @@
+<?php
+
+use JoelButcher\Socialstream\Providers;
+
+return [
+    'middleware' => ['web'],
+    'prompt' => 'Or Login Via',
+    'providers' => [
+        Providers::github(),
+
+    ],
+    'component' => 'socialstream::components.socialstream',
+];
