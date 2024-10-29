88bddc403eb18030d6d76e9773aeb8fa6e639081
Nathalie Castells-Brooke <nathalie.castells@rothamsted.ac.uk>
2024-10-29 11:05:30 +0000

    test line in advanced  technology

--- app/Models/Publication.php
+++ app/Models/Publication.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Publication extends Model
 {
     use HasFactory;
-    protected $table = 'updated_publications';
+    protected $table = 'publications';
     # protected $table = 'publications';
 }
